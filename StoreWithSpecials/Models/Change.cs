58b3bbef721ebe5572f3b7d34e664ed9bd45f59e
Daniel Bowen <danielrbowen@live.com>
2019-01-24 13:38:43 -0700

    Change class for US change.

---
+++ StoreWithSpecials/Models/Change.cs
@@ -0,0 +1,201 @@
+﻿namespace StoreWithSpecials.Models
+{
+    public class Change
+    {
+        public decimal AmountGiven { get; set; }
+
+        public decimal AmountDue { get; set; }
+
+        public decimal ChangeAmount
+        {
+            get
+            {
+                if (AmountGiven >= AmountDue)
+                {
+                    return AmountGiven - AmountDue;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public decimal AmountRemainingDue
+        {
+            get
+            {
+                if (AmountGiven < AmountDue)
+                {
+                    return AmountDue - AmountGiven;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_50
+        {
+            get
+            {
+                if (ChangeAmount >= 50)
+                {
+                    return (int)ChangeAmount / 50;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_20
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50);
+
+                if (remainingChange >= 20)
+                {
+                    return (int)remainingChange / 20;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_10
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20);
+
+                if (remainingChange >= 10)
+                {
+                    return (int)remainingChange / 10;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_5
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10);
+
+                if (remainingChange >= 5)
+                {
+                    return (int)remainingChange / 5;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_1
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10) - (5 * Denomination_5);
+
+                if (remainingChange >= 1)
+                {
+                    return (int)remainingChange / 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_0_25
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10) - (5 * Denomination_5) - (1 * Denomination_1);
+
+                if (remainingChange >= (decimal)0.25)
+                {
+                    return (int)(remainingChange / (decimal)0.25);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_0_10
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10) - (5 * Denomination_5) - (1 * Denomination_1) - ((decimal)0.25 * Denomination_0_25);
+
+                if (remainingChange >= (decimal)0.1)
+                {
+                    return (int)(remainingChange / (decimal)0.1);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_0_05
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10) - (5 * Denomination_5) - (1 * Denomination_1) - ((decimal)0.25 * Denomination_0_25) - ((decimal)0.1 * Denomination_0_10);
+
+                if (remainingChange >= (decimal)0.05)
+                {
+                    return (int)(remainingChange / (decimal)0.05);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public int Denomination_0_01
+        {
+            get
+            {
+                var remainingChange = ChangeAmount - (50 * Denomination_50) - (20 * Denomination_20) - (10 * Denomination_10) - (5 * Denomination_5) - (1 * Denomination_1) - ((decimal)0.25 * Denomination_0_25) - ((decimal)0.1 * Denomination_0_10) - ((decimal)0.05 * Denomination_0_05);
+
+                if (remainingChange >= (decimal)0.01)
+                {
+                    return (int)(remainingChange / (decimal)0.01);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets change for an amount given and an amout due
+        /// </summary>
+        /// <param name="amountGiven"></param>
+        /// <param name="amountDue"></param>
+        public Change(decimal amountGiven, decimal amountDue)
+        {
+            AmountGiven = amountGiven;
+            AmountDue = amountDue;
+        }
+    }
+}
---
+++ StoreWithSpecialsTests/Models/ChangeTest.cs
@@ -0,0 +1,38 @@
+﻿using StoreWithSpecials.Models;
+using Xunit;
+
+namespace StoreWithSpecialsTests
+{
+    public class ChangeTest
+    {
+        [Fact]
+        public void GetChange_One50()
+        {
+            var change = new Change(100, 50);
+            Assert.Equal(1, change.Denomination_50);
+        }
+
+        [Fact]
+        public void GetChange_Given20_Due17_27_Expected2_73()
+        {
+            var change = new Change(20, (decimal)17.27);
+            Assert.Equal((decimal)2.73, change.ChangeAmount);
+            Assert.Equal(0, change.Denomination_50);
+            Assert.Equal(0, change.Denomination_20);
+            Assert.Equal(0, change.Denomination_10);
+            Assert.Equal(0, change.Denomination_5);
+            Assert.Equal(2, change.Denomination_1);
+            Assert.Equal(2, change.Denomination_0_25);
+            Assert.Equal(2, change.Denomination_0_10);
+            Assert.Equal(0, change.Denomination_0_05);
+            Assert.Equal(3, change.Denomination_0_01);
+        }
+
+        [Fact]
+        public void GetChange_Given10_Due17_27_Owed7_27()
+        {
+            var change = new Change(10, (decimal)17.27);
+            Assert.Equal((decimal)7.27, change.AmountRemainingDue);
+        }
+    }
+}
